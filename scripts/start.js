00db53cab22458153bd2dd50afa3be4a753825ea
Rock <82242219@qq.com>
2022-03-14 16:42:44 +0800

    creating

--- scripts/start.js
+++ scripts/start.js
@@ -5,6 +5,9 @@ const fs = require("fs");
 const { merge } = require("lodash");
 const appDirectory = fs.realpathSync(process.cwd());
 
+const http = require("http");
+const esbuild = require("esbuild");
+
 const util = require("util");
 
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
@@ -19,8 +22,13 @@ for (const name in env) {
   }
 }
 
+console.log("entry point >>>", resolveApp("src/index.js"));
+
 const starter = async () => {
-  let config = [{}, {}];
+  let config = [
+    { port: 8000 },
+    { entryPoints: [resolveApp("public/index.js")], loader: { ".js": "jsx" } },
+  ];
 
   const configFileDir = resolveApp("esbuild.override.js");
   let configFile;
@@ -33,12 +41,52 @@ const starter = async () => {
     config = require(configFileDir)(...config);
   }
 
-  require("esbuild")
-    .serve(...config)
-    .then((server) => {
-      // Call "stop" on the web server to stop serving
-      // server.stop()
-    });
+  esbuild.serve(...config).then((result) => {
+    // The result tells us where esbuild's local server is
+    const { host, port } = result;
+
+    http
+      .createServer((req, res) => {
+        const options = {
+          hostname: host,
+          port: port,
+          path: req.url,
+          method: req.method,
+          headers: req.headers,
+        };
+
+        // Forward each incoming request to esbuild
+        const proxyReq = http.request(options, (proxyRes) => {
+          // If esbuild returns "not found", send a custom 404 page
+          if (proxyRes.statusCode === 404) {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(`<!DOCTYPE html>
+          <html lang="en">
+          <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Document</title>
+          </head>
+          <body>
+              <div id="root"></div>
+          
+              <script src="index.js"></script>
+          </body>
+          </html>`);
+            return;
+          }
+
+          // Otherwise, forward the response from esbuild to the client
+          res.writeHead(proxyRes.statusCode, proxyRes.headers);
+          proxyRes.pipe(res, { end: true });
+        });
+
+        // Forward the body of the request to esbuild
+        req.pipe(proxyReq, { end: true });
+      })
+      .listen(3000);
+  });
 };
 
 starter();
