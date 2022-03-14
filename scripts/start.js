718e291a1e88a2ab02fe0760ffc91c3ca7f84f99
Rock <82242219@qq.com>
2022-03-14 14:23:52 +0800

    creating

--- scripts/start.js
+++ scripts/start.js
@@ -13,9 +13,10 @@ const { default: basic, [process.env.APP_ENV]: app_env } = require(res);
 
 const { env } = merge(basic, app_env);
 
-process.env = {
-  ...process.env,
-  ...env.filter((v) => /^(REACT_APP_).*/.test(v)),
-};
+for (const name in env) {
+  if (/^(REACT_APP_).*/.test(env[name])) {
+    process.env[name] = env[name];
+  }
+}
 
 console.log("process.env.REACT_APP_SERVER >>>", process.env.REACT_APP_SERVER);
