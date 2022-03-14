5d80e2f1b1a00b462f390818d909486690636314
Rock <82242219@qq.com>
2022-03-14 14:12:51 +0800

    creating

--- scripts/start.js
+++ scripts/start.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const fs = require('fs');
-const {merge} = require('lodash-es')
+const {merge} = require('lodash')
 const appDirectory = fs.realpathSync(process.cwd());
 
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
