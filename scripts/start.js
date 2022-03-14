54c6195129c7a55fb4af4c39ac649ed62fffd1b8
Rock <82242219@qq.com>
2022-03-14 14:15:41 +0800

    creating

--- scripts/start.js
+++ scripts/start.js
@@ -1,18 +1,21 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const fs = require('fs');
-const {merge} = require('lodash')
+const path = require("path");
+const fs = require("fs");
+const { merge } = require("lodash");
 const appDirectory = fs.realpathSync(process.cwd());
 
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
-const res = resolveApp('app.config.json')
+const res = resolveApp("app.config.json");
 
-const {default: basic, [process.env.APP_ENV]: app_env} = require(res)
+const { default: basic, [process.env.APP_ENV]: app_env } = require(res);
 
-const {env} = merge(basic, app_env)
+const { env } = merge(basic, app_env);
 
-process.env = {...process.env, ...env};
+process.env = {
+  ...process.env,
+  ...env.filter((v) => /^(REACT_APP_).*/.test(v)),
+};
 
-console.log('process.env.REACT_APP_SERVER >>>', process.env.REACT_APP_SERVER);
+console.log("process.env.REACT_APP_SERVER >>>", process.env.REACT_APP_SERVER);
