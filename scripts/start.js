6019f5eb874f3828d0ca616dc5915e3cad16ff0e
Rock <82242219@qq.com>
2022-03-14 14:30:26 +0800

    creating

--- scripts/start.js
+++ scripts/start.js
@@ -8,15 +8,13 @@ const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 const res = resolveApp("app.config.json");
-
 const { default: basic, [process.env.APP_ENV]: app_env } = require(res);
-
 const { env } = merge(basic, app_env);
 
 for (const name in env) {
-  if (/^(REACT_APP_).*/.test(env[name])) {
+  if (/^(REACT_APP_).*/.test(name)) {
     process.env[name] = env[name];
   }
 }
 
-console.log("process.env.REACT_APP_SERVER >>>", process.env.REACT_APP_SERVER);
+console.log("process.env >>>", process.env);
